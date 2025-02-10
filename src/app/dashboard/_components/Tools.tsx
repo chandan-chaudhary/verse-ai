6cf7b2e950c7dc9f90b88441f9256f516557901c
chandan-chaudhary <chandanchaudhary3949@gmail.com>
2025-02-10 20:15:38 +0530

    fixed scrape page

--- src/app/dashboard/_components/Tools.tsx
+++ src/app/dashboard/_components/Tools.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -7,34 +9,35 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 
-import Link from "next/link"
-
 import Image from "next/image";
-// import landingAi from "@/public/ai_landing.jpg";
 import { tools, ToolsProps } from "./tools";
+import { useRouter } from "next/navigation";
 
 export default function Tools() {
+    const router = useRouter();
     return (
-        <div className=" flex flex-col gap-y-8 py-24">
-            <h5 className="text-center text-6xl font-bold underline decoration-wavy">Tools & Features</h5>
-            <div className="grid grid-cols-2 gap-8  ">
-                {
-                    tools.map((tool: ToolsProps, idx: number) =>
-                    (<Card key={idx} className="shadow-2xl">
-                        <CardContent className="flex items-center gap-x-5">
-                            <Image src={tool.image || "/path/to/your/image.jpg"} width={100} height={100} alt="Landing" className="w-fit h-fit object-cover rounded-lg" />
-                            <CardHeader className="text-4xl">
-                                <CardTitle>{tool.title}</CardTitle>
-                                <CardDescription>{tool.description}</CardDescription>
-                                <Link href={`/dashboard/${tool.path}`} className="w-fit self-center mt-8">
-                                    <Button variant={'default'} className="py-4">Get started</Button>
-                                </Link>
+        <div className="flex flex-col gap-y-8 py-24">
+            <h5 className="text-center text-6xl ">Tools & Features</h5>
+            <div className="grid grid-cols-3 gap-8">
+                {tools.map((tool: ToolsProps, idx: number) => (
+                    <Card key={idx} className="shadow-2xl ">
+                        <CardContent className="flex flex-col items-center gap-y-5 p-4">
+                            <Image
+                                src={tool.image || "/path/to/your/image.jpg"}
+                                width={200}
+                                height={200}
+                                alt="Tool Image"
+                                className="w-full h-auto object-cover rounded-lg"
+                            />
+                            <CardHeader className="text-center">
+                                <CardTitle className="text-2xl font-bold">{tool.title}</CardTitle>
+                                <CardDescription className="text-lg">{tool.description}</CardDescription>
+                                <Button onClick={() => router.push(`/dashboard/${tool.path}`)} className="border-white rounded-full text-sm">{tool.buttonContent}</Button>
                             </CardHeader>
                         </CardContent>
-                    </Card>)
-                    )
-                }
+                    </Card>
+                ))}
             </div>
         </div>
-    )
+    );
 }
--- src/app/dashboard/_components/tools.ts
+++ src/app/dashboard/_components/tools.ts
@@ -3,30 +3,36 @@ import landingAi2 from "@/public/ai3.png";
 
 import { StaticImageData } from "next/image";
 
-export interface ToolsProps{
-    title:string;
-    description:string;
-    path:string;
-    image: StaticImageData;
+export interface ToolsProps {
+  title: string;
+  description: string;
+  path: string;
+  image: StaticImageData;
+  buttonContent?: string;
 }
 
-export const tools: ToolsProps[]=[
-    {
-        title:'Generate AI Content',
-        description:"Create Content based on Your ideas, ask question, generate content for your purpose.",
-        path:'content',
-        image:landingAi2
-    },
-    {
-        title:'Prepare for InterView',
-        description:"Practice and prepare for interview, it generate mock interview based on role and description you are applying for.",
-        path:'interview',
-        image:landingAi2
-    },
-    {
-        title:'Start web scraping',
-        description:"scrape web url to generate meaningful insights. Chat with the scraped data to get real-time information and answers.",
-        path:'scrap-data',
-        image:landingAi
-    }
-]
+export const tools: ToolsProps[] = [
+  {
+    title: "Generate AI Content",
+    description:
+      "Create Content based on Your ideas, ask question, generate content for your purpose.",
+    path: "content",
+    image: landingAi2,
+    buttonContent: "Generate Content",
+  },
+  {
+    title: "Prepare for InterView",
+    description:
+      "Practice and prepare for interview, it generate mock interview based on role and description you are applying for.",
+    path: "interview",
+    image: landingAi,
+    buttonContent: "Prepare for Interview",
+  },
+  {
+    title: "Start web scraping",
+    description:"scrape web url to generate meaningful insights. Chat with the scraped data to get real-time information and answers.",
+    path: "scrap-data",
+    image: landingAi2,
+    buttonContent: "Scrape Data",
+  },
+];
